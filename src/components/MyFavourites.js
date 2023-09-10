4252aa607a95a4b8fb53dc4d795264b7873a31f9
Cristina Soare <kiti.soare@ymail.com>
2023-09-10 12:56:28 +0100

    added search bar, home button and My Account menu to TopNavbar

---
+++ src/components/MyFavourites.js
@@ -0,0 +1,8 @@
+//users favourite poses
+import React from "react";
+
+const MyFavourites = () => {
+  return <div>My Favourites Page</div>;
+};
+
+export default MyFavourites;
--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -1,9 +1,20 @@
 // fixed el at top of window. With logo.png (on click takes to home page), Search.js, MyAccount.js with hamburger menu (my poses, my flows, log out)
 
 import React from "react";
-
+import { Link } from "react-router-dom";
 import "../styles/top-nav-bar.scss";
+import SearchBox from "./SearchBox";
+import NavMenu from "./NavMenu";
 
-const TopNavBar = () => {};
+const TopNavBar = () => {
+  return (
+    <nav className="navbar-container">
+      <SearchBox />
+      <Link to="/">HOME</Link>
+      <NavMenu />
+    </nav>
+    //home to be replaced with logo
+  );
+};
 
 export default TopNavBar;
