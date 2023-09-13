5f687b09425e305679c79c73ffd3034f14bfdde8
Cristina Soare <kiti.soare@ymail.com>
2023-09-13 21:17:40 +0100

    connected poses library component to api and added basic styling

--- src/components/app_static/PosesLibrary.js
+++ src/components/app_static/PosesLibrary.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../../styles/app_static_sass/poses-library.scss";
 
 const PosesLibrary = () => {
-	return <div>Poses Library</div>;
+  const [poses, setPoses] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://yoga-api-nzy4.onrender.com/v1/poses")
+      .then((response) => {
+        const yogaPosesArray = response.data;
+        const yogaPictures = yogaPosesArray.map((pose) => pose.url_png);
+        setPoses(yogaPictures);
+        console.log(yogaPictures.length);
+      });
+  }, []);
+
+  const handleClick = () => {
+    //logic for opening modal with card info
+  };
+
+  return (
+    <div>
+      <div className="poses-container">
+        {poses.map((pose, index) => (
+          <div className="grid-item" key={index}>
+            <button class="pose-button" onClick={handleClick}>
+              <img src={pose} alt="yoga-pose" />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PosesLibrary;
