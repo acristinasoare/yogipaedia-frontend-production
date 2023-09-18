1ad6d168a7e0d7b32af8b10f0c4177f6d3021249
Cristina Soare <kiti.soare@ymail.com>
2023-09-18 19:32:49 +0100

    added basic test to PoseOfTheDay

--- src/__tests__/__app_static__/PoseOfTheDay.test.js
+++ src/__tests__/__app_static__/PoseOfTheDay.test.js
@@ -1,1 +1,10 @@
-import PoseOfTheDay from " ../../components/app_static/PoseOfTheDay";
+import React from "react";
+import { render } from "@testing-library/react";
+import PoseOfTheDay from "../../components/app_static/PoseOfTheDay";
+
+describe("PoseOfTheDay", () => {
+  it("renders correctly", () => {
+    const { asFragment } = render(<PoseOfTheDay />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
