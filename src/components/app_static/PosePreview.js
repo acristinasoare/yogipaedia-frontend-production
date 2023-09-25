7969e54f50043111064fd7720f0c013870b5c310
Holly Mathieson <hjmathieson@gmail.com>
2023-09-25 01:24:57 +0100

    level filter now working

--- src/components/app_static/PosePreview.js
+++ src/components/app_static/PosePreview.js
@@ -6,62 +6,64 @@ import PoseDetails from "./PoseDetails";
 import { faHandPointLeft } from "@fortawesome/free-solid-svg-icons";
 
 const PosePreview = ({
-  poseImage,
-  sanskritName,
-  englishName,
-  poseBenefits,
-  poseDescription,
+	poseImage,
+	sanskritName,
+	englishName,
+	poseBenefits,
+	poseDescription,
+	poseLevel,
 }) => {
-  const [isFavourite, setIsFavourite] = useState(false);
+	const [isFavourite, setIsFavourite] = useState(false);
 
-  const handleFavouriteClick = () => {
-    setIsFavourite(!isFavourite);
-    //code for adding into favourites to also be added here
-  };
+	const handleFavouriteClick = () => {
+		setIsFavourite(!isFavourite);
+		//code for adding into favourites to also be added here
+	};
 
-  return (
-    <div className="pose-preview__container">
-      <img className="pose-preview__image" src={poseImage} alt="yoga-pose" />
-      <button
-        className="pose-preview__favourite-button"
-        onClick={handleFavouriteClick}
-      >
-        {isFavourite ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="18"
-            height="18"
-            fill="#ffc2cd"
-            className="bi bi-heart-fill"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
-            />
-          </svg> //svg code copied from bootstrap website
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="18"
-            height="18"
-            fill="#ffc2cd"
-            className="bi bi-heart"
-            viewBox="0 0 16 16"
-          >
-            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-          </svg>
-        )}
-      </button>
-      <PoseDetails
-        poseImage={poseImage}
-        englishName={englishName}
-        sanskritName={sanskritName}
-        poseBenefits={poseBenefits}
-        poseDescription={poseDescription}
-      />
-    </div>
-  );
+	return (
+		<div className="pose-preview__container">
+			<img className="pose-preview__image" src={poseImage} alt="yoga-pose" />
+			<button
+				className="pose-preview__favourite-button"
+				onClick={handleFavouriteClick}
+			>
+				{isFavourite ? (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="18"
+						height="18"
+						fill="#ffc2cd"
+						className="bi bi-heart-fill"
+						viewBox="0 0 16 16"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+						/>
+					</svg> //svg code copied from bootstrap website
+				) : (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="18"
+						height="18"
+						fill="#ffc2cd"
+						className="bi bi-heart"
+						viewBox="0 0 16 16"
+					>
+						<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+					</svg>
+				)}
+			</button>
+			<PoseDetails
+				poseImage={poseImage}
+				englishName={englishName}
+				sanskritName={sanskritName}
+				poseBenefits={poseBenefits}
+				poseDescription={poseDescription}
+				poseLevel={poseLevel}
+			/>
+		</div>
+	);
 };
 
 export default PosePreview;
--- src/components/app_static/PosesLibrary.js
+++ src/components/app_static/PosesLibrary.js
@@ -7,91 +7,116 @@ import PosePreview from "./PosePreview";
 import Alert from "./Alert";
 
 const PosesLibrary = () => {
-  const [poses, setPoses] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState(null);
-  const [alertMessage, setAlertMessage] = useState(null);
+	const [poses, setPoses] = useState([]);
+	const [selectedCategory, setSelectedCategory] = useState(null);
+	const [alertMessage, setAlertMessage] = useState(null);
+	const [selectedLevel, setSelectedLevel] = useState(null);
+	let yogaPosesArray;
 
-  const handleSelectedCategory = (category) => {
-    setSelectedCategory(category);
-  };
-  const handleClearFilter = () => {
-    setSelectedCategory(null);
-  };
+	const handleSelectedCategory = (category) => {
+		setSelectedCategory(category);
+	};
+	const handleClearFilter = () => {
+		setSelectedCategory(null);
+	};
 
-  useEffect(() => {
-    let endpoint = "/poses";
+	const handleSelectedLevel = (level) => {
+		setSelectedLevel(level);
+	};
 
-    if (selectedCategory) {
-      endpoint = `/poses/category/${selectedCategory}`;
-    }
+	useEffect(() => {
+		let endpoint = "/poses";
 
-    axios
-      .get(endpoint)
-      .then((response) => {
-        console.log(response);
-        const yogaPosesArray = response.data;
-        console.log(yogaPosesArray);
-        const yogaPictures = yogaPosesArray.map((pose) => [
-          pose.url_png,
-          pose.pose_name,
-          pose.sanskrit_name,
-          pose.pose_benefits,
-          pose.pose_description,
-        ]);
-        setPoses(yogaPictures);
-        console.log(yogaPictures);
-        console.log(yogaPictures.length);
-      })
-      .catch(() => setAlertMessage("Server Error. Please try again later."));
-  }, [selectedCategory]);
+		if (selectedCategory) {
+			endpoint = `/poses/category/${selectedCategory}`;
+		}
 
-  if (alertMessage) {
-    return (
-      <div>
-        <div className="top-nav-bar">
-          <TopNavBar />
-        </div>
-        <SideNavBar
-          handleSelectedCategory={handleSelectedCategory}
-          handleClearFilter={handleClearFilter}
-        />
-        <div className="alert-message">
-          <Alert message={alertMessage} />
-        </div>
-      </div>
-    );
-  }
+		axios
+			.get(endpoint)
+			.then((response) => {
+				yogaPosesArray = response.data;
 
-  return (
-    <div>
-      <div className="top-nav-bar">
-        <TopNavBar />
-      </div>
-      <div className="side-bar">
-        <SideNavBar
-          handleSelectedCategory={handleSelectedCategory}
-          handleClearFilter={handleClearFilter}
-        />
-      </div>
-      <div className="page-content__container">
-        <div className="poses__library">
-          <div className="poses__container">
-            {poses.map((pose, index) => (
-              <div className="grid-item" key={index} onClick={() => {}}>
-                <PosePreview
-                  poseImage={pose[0]}
-                  englishName={pose[1]}
-                  sanskritName={pose[2]}
-                  poseBenefits={pose[3]}
-                  poseDescription={pose[4]}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+				const filteredPictures = yogaPosesArray
+					.filter((pose) => pose.level === selectedLevel)
+					.map((pose) => [
+						pose.url_png,
+						pose.pose_name,
+						pose.sanskrit_name,
+						pose.pose_benefits,
+						pose.pose_description,
+					]);
+
+				const yogaPictures = yogaPosesArray.map((pose) => [
+					pose.url_png,
+					pose.pose_name,
+					pose.sanskrit_name,
+					pose.pose_benefits,
+					pose.pose_description,
+				]);
+
+				if (selectedLevel) {
+					setPoses(filteredPictures);
+				} else {
+					setPoses(yogaPictures);
+					console.log(`yoga pictures: `);
+					console.log(yogaPictures);
+					console.log(`filtered pictures:`);
+					console.log(filteredPictures);
+				}
+			})
+			.catch(() => setAlertMessage("Server Error. Please try again later."));
+	}, [selectedCategory, selectedLevel]);
+
+	if (alertMessage) {
+		return (
+			<div>
+				<div className="top-nav-bar">
+					<TopNavBar />
+				</div>
+				<SideNavBar
+					handleSelectedCategory={handleSelectedCategory}
+					handleClearFilter={handleClearFilter}
+					handleSelectedLevel={handleSelectedLevel}
+				/>
+				<div className="alert-message">
+					<Alert message={alertMessage} />
+				</div>
+			</div>
+		);
+	}
+
+	return (
+		<div>
+			<div className="top-nav-bar">
+				<TopNavBar />
+			</div>
+			<div className="side-bar">
+				<SideNavBar
+					handleSelectedCategory={handleSelectedCategory}
+					handleClearFilter={handleClearFilter}
+					handleSelectedLevel={handleSelectedLevel}
+				/>
+			</div>
+			<div className="page-content__container">
+				<div className="poses__library">
+					<div className="poses__container">
+						{poses.map((pose, index) => (
+							<div className="grid-item" key={index} onClick={() => {}}>
+								<PosePreview
+									poseImage={pose[0]}
+									englishName={pose[1]}
+									sanskritName={pose[2]}
+									poseBenefits={pose[3]}
+									poseDescription={pose[4]}
+									poseLevel={pose[5]}
+								/>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default PosesLibrary;
