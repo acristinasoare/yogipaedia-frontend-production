29521a2d00d49e2571b3318831f56774fbdcccb3
Cristina Soare <kiti.soare@ymail.com>
2023-09-29 18:13:42 +0100

    tidied up components code and scss

--- src/components/authorisation/SignUpPage.js
+++
@@ -1,9 +0,0 @@
-// renders onClick from button on login modal. includes two areas: SignUp.js box and Taster. Format as row on desktop, column on mobile
-
-import React from "react";
-
-import "../styles/sign-up-page.scss";
-
-const SignUpPage = () => {};
-
-export default SignUpPage;
--- src/components/authorisation/Taster.js
+++
@@ -1,9 +0,0 @@
-// Preview/information for app offerings
-
-import React from "react";
-
-import "../styles/taster.scss";
-
-const Taster = () => {};
-
-export default Taster;
