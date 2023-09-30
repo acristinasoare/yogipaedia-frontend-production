99a9ddb3786212841c7bd50277134803ba5069ec
Cristina Soare <kiti.soare@ymail.com>
2023-09-30 19:17:51 +0100

    removed Add favourites button from Modal and My Favourite Title renders again

--- src/components/app_static/PoseDetails.js
+++ src/components/app_static/PoseDetails.js
@@ -3,7 +3,6 @@
 
 import * as React from "react";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
@@ -14,97 +13,83 @@ import "../../styles/app_static_sass/pose-details.scss";
 
 // MUI modal inbuilt style settings
 const style = {
-	position: "absolute",
-	top: "50%",
-	left: "50%",
-	transform: "translate(-50%, -50%)",
-	width: 300,
-	bgcolor: "background.paper",
-	border: "2px solid #000",
-	boxShadow: 24,
-	p: 4,
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 300,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
 };
 
 const PoseDetails = ({
-	userId,
-	poseId,
-	poseImage,
-	sanskritName,
-	englishName,
-	poseBenefits,
-	poseDescription,
-	poseLevel,
-	handleFavouriteClick,
+  userId,
+  poseId,
+  poseImage,
+  sanskritName,
+  englishName,
+  poseBenefits,
+  poseDescription,
+  poseLevel,
+  handleFavouriteClick,
 }) => {
-	const [open, setOpen] = React.useState(false);
-	const handleOpen = () => setOpen(true);
-	const handleClose = () => setOpen(false);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-	return (
-		<div>
-			<Button className="pose__modal-button" onClick={handleOpen}>
-				{englishName}
-			</Button>
-			<Modal
-				className="pose__modal"
-				open={open}
-				onClose={handleClose}
-				aria-labelledby="modal-modal-title"
-				aria-describedby="modal-modal-description"
-			>
-				<Card className="pose__card" sx={{ style }}>
-					<CardMedia
-						className="pose__image"
-						component="img"
-						sx={{ height: 250, width: 250 }}
-						image={poseImage}
-						title="green iguana"
-					/>
-					<CardContent className="pose__text-areas">
-						<Typography
-							className="pose__names"
-							gutterBottom
-							variant="h5"
-							component="div"
-						>
-							{englishName} / {sanskritName}
-						</Typography>
-						<br />
-						<Typography
-							className="pose__description"
-							component="div"
-							variant="body2"
-							color="text.secondary"
-						>
-							{poseDescription}
-						</Typography>
-						<br />
-						<Typography
-							className="pose__benefits"
-							component="div"
-							variant="body2"
-							color="text.secondary"
-						>
-							Benefits: {poseBenefits}
-						</Typography>
-					</CardContent>
-					<CardActions>
-						<Button
-							className="card__button"
-							size="small"
-							onClick={() =>
-								handleFavouriteClick(
-									(userId = { userId }),
-									(poseId = { poseId })
-								)
-							}
-						>
-							Add to my favourites
-						</Button>
-					</CardActions>
-				</Card>
-			</Modal>
-		</div>
-	);
+  return (
+    <div>
+      <Button className="pose__modal-button" onClick={handleOpen}>
+        {englishName}
+      </Button>
+      <Modal
+        className="pose__modal"
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Card className="pose__card" sx={{ style }}>
+          <CardMedia
+            className="pose__image"
+            component="img"
+            sx={{ height: 250, width: 250 }}
+            image={poseImage}
+            title="green iguana"
+          />
+          <CardContent className="pose__text-areas">
+            <Typography
+              className="pose__names"
+              gutterBottom
+              variant="h5"
+              component="div"
+            >
+              {englishName} / {sanskritName}
+            </Typography>
+            <br />
+            <Typography
+              className="pose__description"
+              component="div"
+              variant="body2"
+              color="text.secondary"
+            >
+              {poseDescription}
+            </Typography>
+            <br />
+            <Typography
+              className="pose__benefits"
+              component="div"
+              variant="body2"
+              color="text.secondary"
+            >
+              Benefits: {poseBenefits}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Modal>
+    </div>
+  );
 };
 export default PoseDetails;
--- src/components/user_account/MyFavourites.js
+++ src/components/user_account/MyFavourites.js
@@ -4,18 +4,18 @@ import MyPoses from "./MyPoses";
 import "../../styles/user_account_sass/my-favourites.scss";
 
 const MyFavourites = ({ userId }) => {
-	return (
-		<div>
-			<div className="top-nav-bar">
-				<TopNavBar />
-			</div>
-			<div className="page-content__container">
-				<div className="page-content__title">
-					<h2 className="title-text">My Favourite Poses</h2>
-				</div>
-				<MyPoses userId={userId} />
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="top-nav-bar">
+        <TopNavBar />
+      </div>
+      <div className="page-content__container">
+        <div className="page-content__title">
+          <h2 className="title-text">My Favourite Poses</h2>
+        </div>
+        <MyPoses userId={userId} />
+      </div>
+    </div>
+  );
 };
 export default MyFavourites;
