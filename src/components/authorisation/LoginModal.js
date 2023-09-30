24d19220d3b2bc6e42b4deb78c656a5d67888911
Cristina Soare <kiti.soare@ymail.com>
2023-09-30 16:50:04 +0100

    corrected spelling of Yogipaedia

--- src/components/authorisation/LoginModal.js
+++ src/components/authorisation/LoginModal.js
@@ -44,7 +44,7 @@ const LoginModal = ({ setUserId, userId }) => {
     <>
       <div className="page-content__container">
         <div className="p-4 box">
-          <h2 className="mb-3"> Yogipaedia App Auth </h2>
+          <h2 className="mb-3"> Yogipeadia App Auth </h2>
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
