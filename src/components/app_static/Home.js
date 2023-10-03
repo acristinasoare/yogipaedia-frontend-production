571bfcfb3afd3f89360720f1ef5aeca0ccc737d3
Holly Mathieson <hjmathieson@gmail.com>
2023-10-03 21:49:53 +0100

    changed proxy

--- src/components/app_static/Home.js
+++ src/components/app_static/Home.js
@@ -2,17 +2,10 @@ import React, { useState } from "react";
 import PoseOfTheDay from "./PoseOfTheDay";
 import TopNavBar from "../nav_and_search/TopNavBar";
 import Footer from "./Footer";
-import { auth } from "../../config/firebase";
 
 import "../../styles/app_static_sass/home.scss";
 
 const Home = ({ currentUser, userId }) => {
-	// const [userId, setUserId] = useState(null);
-	// const [currentUser, setCurrentUser] = useState("");
-	// setCurrentUser(localStorage.getItem("currentUser"));
-
-	// const currentUser = localStorage.getItem("currentUser");
-
 	console.log(`home.js current user is ${currentUser}`);
 	console.log(`home.js userId is ${userId}`);
 
