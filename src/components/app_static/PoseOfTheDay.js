adc3142f4cd4969b685623409bc764a0daebf2a4
Cristina Soare <kiti.soare@ymail.com>
2024-02-10 10:16:10 +0000

    created backend-host config file and updated endpoints accordingly

--- src/components/app_static/PoseOfTheDay.js
+++ src/components/app_static/PoseOfTheDay.js
@@ -4,133 +4,134 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../../styles/app_static_sass/pose-of-the-day.scss";
 import Alert from "./Alert";
+import backendHost from "../../config/backend-host";
 
 const PoseOfTheDay = ({ userId }) => {
-	const [randomImage, setRandomImage] = useState(null);
-	const [poseName, setPoseName] = useState(null);
-	const [alertMessage, setAlertMessage] = useState(null);
+  const [randomImage, setRandomImage] = useState(null);
+  const [poseName, setPoseName] = useState(null);
+  const [alertMessage, setAlertMessage] = useState(null);
 
-	useEffect(() => {
-		const lastGeneratedDate = localStorage.getItem("lastGeneratedDate");
-		const currentDate = new Date().toDateString();
+  useEffect(() => {
+    const lastGeneratedDate = localStorage.getItem("lastGeneratedDate");
+    const currentDate = new Date().toDateString();
 
-		if (lastGeneratedDate !== currentDate) {
-			axios
-				.get("https://yogipaedia-bj0y.onrender.com/poses")
-				.then((response) => {
-					const yogaPosesArray = response.data;
-					console.log(yogaPosesArray);
-					const randomIndex = Math.floor(Math.random() * yogaPosesArray.length);
-					const randomPose = yogaPosesArray[randomIndex];
-					const randomImageUrl = randomPose.url_png;
-					const randomName = randomPose.pose_name;
+    if (lastGeneratedDate !== currentDate) {
+      axios
+        .get(`${backendHost}/poses`)
+        .then((response) => {
+          const yogaPosesArray = response.data;
+          console.log(yogaPosesArray);
+          const randomIndex = Math.floor(Math.random() * yogaPosesArray.length);
+          const randomPose = yogaPosesArray[randomIndex];
+          const randomImageUrl = randomPose.url_png;
+          const randomName = randomPose.pose_name;
 
-					console.log(randomPose);
-					console.log(randomImageUrl);
-					console.log(randomName);
+          console.log(randomPose);
+          console.log(randomImageUrl);
+          console.log(randomName);
 
-					setRandomImage(randomImageUrl);
-					setPoseName(randomName);
+          setRandomImage(randomImageUrl);
+          setPoseName(randomName);
 
-					localStorage.setItem("lastGeneratedDate", currentDate);
-					localStorage.setItem("lastGeneratedPictureUrl", randomImageUrl);
-					localStorage.setItem("lastGeneratedName", randomName);
-				})
-				.catch(() =>
-					setAlertMessage("Server error. Pose of the day data unavailable.")
-				);
-		} else {
-			const storedPictureUrl = localStorage.getItem("lastGeneratedPictureUrl");
-			const storedName = localStorage.getItem("lastGeneratedName");
-			setRandomImage(storedPictureUrl);
-			setPoseName(storedName);
-		}
-	}, []);
+          localStorage.setItem("lastGeneratedDate", currentDate);
+          localStorage.setItem("lastGeneratedPictureUrl", randomImageUrl);
+          localStorage.setItem("lastGeneratedName", randomName);
+        })
+        .catch(() =>
+          setAlertMessage("Server error. Pose of the day data unavailable.")
+        );
+    } else {
+      const storedPictureUrl = localStorage.getItem("lastGeneratedPictureUrl");
+      const storedName = localStorage.getItem("lastGeneratedName");
+      setRandomImage(storedPictureUrl);
+      setPoseName(storedName);
+    }
+  }, []);
 
-	// 			.then((response) => {
-	// 				console.log(response);
-	// 				console.log(response.data);
-	// 				const yogaPosesArray = response.data;
-	// 				console.log(yogaPosesArray);
-	// 				const randomIndex = Math.floor(Math.random() * yogaPosesArray.length);
-	// 				const randomPose = yogaPosesArray[randomIndex];
-	// 				const randomImageUrl = randomPose.url_png;
-	// 				const randomName = randomPose.pose_name;
+  // 			.then((response) => {
+  // 				console.log(response);
+  // 				console.log(response.data);
+  // 				const yogaPosesArray = response.data;
+  // 				console.log(yogaPosesArray);
+  // 				const randomIndex = Math.floor(Math.random() * yogaPosesArray.length);
+  // 				const randomPose = yogaPosesArray[randomIndex];
+  // 				const randomImageUrl = randomPose.url_png;
+  // 				const randomName = randomPose.pose_name;
 
-	// 				console.log(randomPose);
-	// 				console.log(randomImageUrl);
-	// 				console.log(randomName);
+  // 				console.log(randomPose);
+  // 				console.log(randomImageUrl);
+  // 				console.log(randomName);
 
-	// 				setRandomImage(randomImageUrl);
-	// 				setPoseName(randomName);
+  // 				setRandomImage(randomImageUrl);
+  // 				setPoseName(randomName);
 
-	// 				localStorage.setItem("lastGeneratedDate", currentDate);
-	// 				localStorage.setItem("lastGeneratedPictureUrl", randomImageUrl);
-	// 				localStorage.setItem("lastGeneratedName", randomName);
-	// 			})
-	// 			.catch(() =>
-	// 				setAlertMessage("Server error. Pose of the day data unavailable.")
-	// 			);
-	// 	} else {
-	// 		const storedPictureUrl = localStorage.getItem("lastGeneratedPictureUrl");
-	// 		const storedName = localStorage.getItem("lastGeneratedName");
-	// 		setRandomImage(storedPictureUrl);
-	// 		setPoseName(storedName);
-	// 	}
-	// }, []);
+  // 				localStorage.setItem("lastGeneratedDate", currentDate);
+  // 				localStorage.setItem("lastGeneratedPictureUrl", randomImageUrl);
+  // 				localStorage.setItem("lastGeneratedName", randomName);
+  // 			})
+  // 			.catch(() =>
+  // 				setAlertMessage("Server error. Pose of the day data unavailable.")
+  // 			);
+  // 	} else {
+  // 		const storedPictureUrl = localStorage.getItem("lastGeneratedPictureUrl");
+  // 		const storedName = localStorage.getItem("lastGeneratedName");
+  // 		setRandomImage(storedPictureUrl);
+  // 		setPoseName(storedName);
+  // 	}
+  // }, []);
 
-	if (alertMessage) {
-		return (
-			<div className="pose-of-the-day__container">
-				<div className="pose__container">
-					<div className="pose-of-the-day">
-						<h4 className="pose-of-the-day__alert-message">
-							<Alert message={alertMessage} />
-						</h4>
-					</div>
-				</div>
-				<div className="pose-description__container">
-					<p>
-						Yoga is a holistic practice that originated in ancient India and
-						encompases physical postures, breathing exercises, meditation, and
-						ethical principles. It is a discipline aimed at promoting physical,
-						mental, and spiritual well-being. Through the practice of yoga,
-						individuals can improve flexibility, strength, and balance, while
-						also reducing stress, enhancing mental clarity and fostering a sense
-						of inner peace. Whether you are a beginner yogi or an aspiring yoga
-						teacher, you can explore a variety of yoga poses by using the
-						Yogipeadia app.
-					</p>
-				</div>
-			</div>
-		);
-	}
-	return (
-		<div className="pose-of-the-day__container">
-			<div className="pose__container">
-				<div className="pose-of-the-day">
-					<h2>Pose Of The Day</h2>
-					<h4>{poseName}</h4>
-				</div>
-				<div className="pose__image">
-					<img src={randomImage} alt="yoga pose of the day" />
-				</div>
-			</div>
-			<div className="pose-description__container">
-				<p>
-					Yoga is a holistic practice that originated in ancient India and
-					encompases physical postures, breathing exercises, meditation, and
-					ethical principles. It is a discipline aimed at promoting physical,
-					mental, and spiritual well-being. Through the practice of yoga,
-					individuals can improve flexibility, strength, and balance, while also
-					reducing stress, enhancing mental clarity and fostering a sense of
-					inner peace. Whether you are a beginner yogi or an aspiring yoga
-					teacher, you can explore a variety of yoga poses by using the
-					Yogipeadia app.
-				</p>
-			</div>
-		</div>
-	);
+  if (alertMessage) {
+    return (
+      <div className="pose-of-the-day__container">
+        <div className="pose__container">
+          <div className="pose-of-the-day">
+            <h4 className="pose-of-the-day__alert-message">
+              <Alert message={alertMessage} />
+            </h4>
+          </div>
+        </div>
+        <div className="pose-description__container">
+          <p>
+            Yoga is a holistic practice that originated in ancient India and
+            encompases physical postures, breathing exercises, meditation, and
+            ethical principles. It is a discipline aimed at promoting physical,
+            mental, and spiritual well-being. Through the practice of yoga,
+            individuals can improve flexibility, strength, and balance, while
+            also reducing stress, enhancing mental clarity and fostering a sense
+            of inner peace. Whether you are a beginner yogi or an aspiring yoga
+            teacher, you can explore a variety of yoga poses by using the
+            Yogipeadia app.
+          </p>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="pose-of-the-day__container">
+      <div className="pose__container">
+        <div className="pose-of-the-day">
+          <h2>Pose Of The Day</h2>
+          <h4>{poseName}</h4>
+        </div>
+        <div className="pose__image">
+          <img src={randomImage} alt="yoga pose of the day" />
+        </div>
+      </div>
+      <div className="pose-description__container">
+        <p>
+          Yoga is a holistic practice that originated in ancient India and
+          encompases physical postures, breathing exercises, meditation, and
+          ethical principles. It is a discipline aimed at promoting physical,
+          mental, and spiritual well-being. Through the practice of yoga,
+          individuals can improve flexibility, strength, and balance, while also
+          reducing stress, enhancing mental clarity and fostering a sense of
+          inner peace. Whether you are a beginner yogi or an aspiring yoga
+          teacher, you can explore a variety of yoga poses by using the
+          Yogipeadia app.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default PoseOfTheDay;
--- src/components/user_account/MyPoses.js
+++ src/components/user_account/MyPoses.js
@@ -4,76 +4,77 @@ import Alert from "../app_static/Alert";
 import PosePreview from "../app_static/PosePreview";
 import "../../styles/user_account_sass/my-poses.scss";
 import FavouriteButton from "../app_static/FavouriteButton";
+import backendHost from "../../config/backend-host";
 
 const MyPoses = ({ userId, currentUser }) => {
-	const [poses, setPoses] = useState([]);
-	const [alertMessage, setAlertMessage] = useState(null);
+  const [poses, setPoses] = useState([]);
+  const [alertMessage, setAlertMessage] = useState(null);
 
-	useEffect(() => {
-		// const currentUser = localStorage.getItem("currentUser");
-		let endpoint = `https://yogipaedia-bj0y.onrender.com/favourites/${userId}`;
+  useEffect(() => {
+    // const currentUser = localStorage.getItem("currentUser");
+    let endpoint = `${backendHost}/favourites/${userId}`;
 
-		axios
-			.get(endpoint)
-			.then((response) => {
-				const myFavouritesArray = response.data;
-				console.log(myFavouritesArray);
-				const myFavouritesPictures = myFavouritesArray.map((pose) => [
-					pose.pose_id,
-					pose.url_png,
-					pose.pose_name,
-					pose.sanskrit_name,
-					pose.pose_benefits,
-					pose.pose_description,
-				]);
-				setPoses(myFavouritesPictures);
-			})
-			.catch(() =>
-				setAlertMessage(
-					"Server error. Unable to retrieve user's favourite poses."
-				)
-			);
-	}, []);
+    axios
+      .get(endpoint)
+      .then((response) => {
+        const myFavouritesArray = response.data;
+        console.log(myFavouritesArray);
+        const myFavouritesPictures = myFavouritesArray.map((pose) => [
+          pose.pose_id,
+          pose.url_png,
+          pose.pose_name,
+          pose.sanskrit_name,
+          pose.pose_benefits,
+          pose.pose_description,
+        ]);
+        setPoses(myFavouritesPictures);
+      })
+      .catch(() =>
+        setAlertMessage(
+          "Server error. Unable to retrieve user's favourite poses."
+        )
+      );
+  }, [userId]);
 
-	if (alertMessage) {
-		return (
-			<div className="alert-message">
-				<Alert message={alertMessage} />
-			</div>
-		);
-	}
+  if (alertMessage) {
+    return (
+      <div className="alert-message">
+        <Alert message={alertMessage} />
+      </div>
+    );
+  }
 
-	return (
-		<div className="my-favourites__container">
-			<div className="my-favourites__title">
-				<h2 className="title-text">My Favourite Poses</h2>
-			</div>
-			<div className="my-poses__container">
-				{poses.map((pose, index) => (
-					<div className="my-poses__grid-item" key={index} onClick={() => {}}>
-						<FavouriteButton
-							className="my-poses__grid-item--button"
-							currentUser={currentUser}
-							userId={userId}
-							poseId={pose[0]}
-						/>
-						<PosePreview
-							className="my-poses__grid-item--pose"
-							currentUser={currentUser}
-							poseId={pose[0]}
-							userId={userId}
-							poseImage={pose[1]}
-							englishName={pose[2]}
-							sanskritName={pose[3]}
-							poseBenefits={pose[4]}
-							poseDescription={pose[5]}
-							poseLevel={pose[6]}
-						/>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="my-favourites__container">
+      <div className="my-favourites__title">
+        <h2 className="title-text">My Favourite Poses</h2>
+      </div>
+      <div className="my-poses__container">
+        {poses.map((pose, index) => (
+          <div className="my-poses__grid-item" key={index} onClick={() => {}}>
+            <FavouriteButton
+              className="my-poses__grid-item--button"
+              currentUser={currentUser}
+              userId={userId}
+              poseId={pose[0]}
+            />
+            <PosePreview
+              className="my-poses__grid-item--pose"
+              currentUser={currentUser}
+              poseId={pose[0]}
+              userId={userId}
+              poseImage={pose[1]}
+              englishName={pose[2]}
+              sanskritName={pose[3]}
+              poseBenefits={pose[4]}
+              poseDescription={pose[5]}
+              poseLevel={pose[6]}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyPoses;
